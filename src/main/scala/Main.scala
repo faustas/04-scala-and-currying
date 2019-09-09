a7e59553b388e89afe45b3fef88b971cf4eaf0e6
André Schütz <andre@wegtam.com>
2019-09-09 13:16:04 +0200

    Examples for currying

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,6 +10,8 @@ object Main {
     // We create a new function that expects two integer values
     // we only have to provide the strings once
     val newFunc: (Int, Int) => String = appendAndSumCurried("Sum of", "and", "is:")
+    // We can pass this function to parts of the code, where the required parameters are available.
+    val result = newFunc(3, 3)
     println(newFunc(1, 1))
     println(newFunc(1, 2))
 
